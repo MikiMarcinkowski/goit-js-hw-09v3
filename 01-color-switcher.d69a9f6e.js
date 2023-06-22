ff64bb022673d5e6910a8e427d395d6426be1075
mikus151 <113313423+mikus151@users.noreply.github.com>
2023-06-22 21:59:15 +0000

    Deploying to gh-pages from @ mikus151/goit-js-hw-09v3@7a7194196f0b04fb60de6fc3ca1155c41cb3062f 🚀

--- 01-color-switcher.d69a9f6e.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector("body");var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;t.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.d69a9f6e.js.map
