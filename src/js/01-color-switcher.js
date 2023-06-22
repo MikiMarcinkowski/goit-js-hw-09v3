351489324cde876b93236e34bf1efb62e260cd7b
mikus151 <mikus151@wp.pl>
2023-06-22 23:12:58 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,4 +2,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+
+document.addEventListener('click', changeBgColor);
+
+function changeBgColor() {
+  
+    document.body.style.backgroundColor = getRandomHexColor();
+
+};
