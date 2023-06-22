22882128135f8137c8ceb630149c238c4f2a05b1
mikus151 <mikus151@wp.pl>
2023-06-23 00:45:51 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,8 +4,17 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const input = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+startBtn.disabled = true;
 
-const fp = flatpickr(input, {
-    enableTime: true,
-    dateFormat: "Y-m-d H:i",
-}); 
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+const fp = flatpickr(input, options);
+
