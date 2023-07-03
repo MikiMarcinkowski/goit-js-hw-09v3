f52875ed2db29316b0401e2d092a83b731e0cc37
mikus151 <mikus151@wp.pl>
2023-07-04 00:52:34 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+1;
+const form = document.querySelector('.form');
+// const elements = form.values.elements;
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  const formData = {
+    delay: form.elements.delay.value,
+    step: form.elements.step.value,
+    amount: form.elements.amount.value,
+  };
+
+  for (let i = 1; i <= formData.amount; i += 1) {
+    let delay = formData.delay
+    createPromise(i, delay )
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += formData.step;
   }
 }
