36b80480e7c51b6d6b082918c7e84244312d60f3
mikus151 <mikus151@wp.pl>
2023-07-05 00:39:49 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -39,21 +39,14 @@ const options = {
         const timeToLeft = dateToCountdown - actualDate;
         console.log(timeToLeft);
 
-        if (timeToLeft <= 1) {
-          clearInterval(timerId);
-          daysToLeft.textContent = '00';
-          hoursToLeft.textContent = '00';
-          minutesToLeft.textContent = '00';
-          secondsToLeft.textContent = '00';
-        }
-
+        
         function convertMs(ms) {
           // Number of milliseconds per unit of time
           const second = 1000;
           const minute = second * 60;
           const hour = minute * 60;
           const day = hour * 24;
-
+          
           // Remaining days
           const days = Math.floor(ms / day);
           // Remaining hours
@@ -62,31 +55,39 @@ const options = {
           const minutes = Math.floor(((ms % day) % hour) / minute);
           // Remaining seconds
           const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
+          
           return { days, hours, minutes, seconds };
         }
-
+        
         console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
         console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
         console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
+        
         daysToLeft.textContent =
-          convertMs(timeToLeft).days < 10
-            ? '0' + convertMs(timeToLeft).days
-            : convertMs(timeToLeft).days;
+        convertMs(timeToLeft).days < 10
+        ? '0' + convertMs(timeToLeft).days
+        : convertMs(timeToLeft).days;
         hoursToLeft.textContent =
-          convertMs(timeToLeft).hours < 10
-            ? '0' + convertMs(timeToLeft).hours
-            : convertMs(timeToLeft).hours;
+        convertMs(timeToLeft).hours < 10
+        ? '0' + convertMs(timeToLeft).hours
+        : convertMs(timeToLeft).hours;
         minutesToLeft.textContent =
-          convertMs(timeToLeft).minutes < 10
-            ? '0' + convertMs(timeToLeft).minutes
-            : convertMs(timeToLeft).minutes;
+        convertMs(timeToLeft).minutes < 10
+        ? '0' + convertMs(timeToLeft).minutes
+        : convertMs(timeToLeft).minutes;
         secondsToLeft.textContent =
-          convertMs(timeToLeft).seconds < 10
-            ? '0' + convertMs(timeToLeft).seconds
-            : convertMs(timeToLeft).seconds;
+        convertMs(timeToLeft).seconds < 10
+        ? '0' + convertMs(timeToLeft).seconds
+        : convertMs(timeToLeft).seconds;
       }, 1000);
+      
+      if (timeToLeft <= 1) {
+        clearInterval(timerId);
+        daysToLeft.textContent = '00';
+        hoursToLeft.textContent = '00';
+        minutesToLeft.textContent = '00';
+        secondsToLeft.textContent = '00';
+      }
       startBtn.disabled = true;
       startBtn.remove();
     }
